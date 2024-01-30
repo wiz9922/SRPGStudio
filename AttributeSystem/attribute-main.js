8a056c9fe5cf8d0054487e325c225f3a1924a61c
wiz9922 <wizjp22@gmail.com>
2024-01-30 23:58:44 +0900

    update

--- AttributeSystem/attribute-main.js
+++ AttributeSystem/attribute-main.js
@@ -2,9 +2,13 @@
 属性によるダメージ変化を設定します。
 
 使用方法:
-ユニット、クラス、アイテム、スキル、地形、ステートにカスタムパラメータを設定してください。
-未設定時は0として扱われます。
+1.下記「_Attribute」に、属性を設定してください。
+{name:～, show:～}の形式をカンマ区切りで追加することで新しい属性を設定することができます。
 
+2.ユニット、クラス、武器、アイテム、スキル、地形、ステートにカスタムパラメータを設定してください。
+未設定時は0として扱われます。そのため_Attributeの一番上(0番目)は無属性であることが推奨されます。
+
+例
 {
 	attribute: {
 		type: 2,
@@ -15,23 +19,24 @@
 }
 
 (それぞれ省略可)
-type: 付与する攻撃属性の種類(_AttrNameの順に0,1,2,...)
+type: 付与する攻撃属性の番号(_Attributeの上から0,1,2,...とする)
 pow_n: n番目の属性強化値
 res_n: n番目の属性耐性値
 
-Q.属性を増やしたい！減らしたい！名前変えたい！
-A._Attribute内を変更してください
+3.下記「_Priority」に、複数の攻撃属性が付与される際の優先度を設定(項目の順番を変更)してください。
+例えば初期状態では、武器の属性よりスキルの属性が優先されます。
 
 ■作成者
 wiz
 
 ■対応バージョン
-SRPG Stduio Version:1.111
+SRPG Stduio Version:1.289
 
 ■更新履歴
 2017/01/20	作成
 2017/01/23	地形に対応、アイテム情報の項目追加、非表示設定追加
 2020/03/23	ダメージがマイナスの時に調整されていなかったのを修正
+2024/01/31	攻撃属性付与の優先度を追加
 
 --------------------------------------------------------------------------------------------------*/
 var AttrParamType = {
@@ -52,23 +57,32 @@ var AttributeControl = {
 	show:表示するものはtrue、非表示しないものはfalse
 	-------------------------------------------------*/
 	_Attribute: [
-		{name: '無', show: true},
-		{name: '火', show: true},
-		{name: '水', show: true},
-		{name: '風', show: true},
-		{name: '地', show: true}
+		{name: '無', show: true}
+		,{name: '火', show: true}
+		,{name: '水', show: true}
+		,{name: '風', show: true}
+		,{name: '地', show: true}
 	],
 	
-	//objのnullチェック+属性の設定がされているか
-	_isAttributeCustom: function(obj) {
-		if(obj == null) {
-			return false;
-		}
-		if(typeof obj.custom.attribute === 'undefined') {
-			return false;
-		}
-		return true;
-	},
+	/*------------------------------------------------
+	攻撃属性付与の優先度(下の項目ほど優先)
+	ObjectType.UNIT:ユニット
+	ObjectType.CLS:クラス
+	ObjectType.WEAPON:武器
+	ObjectType.ITEM:アイテム
+	ObjectType.SKILL:スキル
+	ObjectType.TERRAIN:地形
+	ObjectType.STATE:ステート
+	-------------------------------------------------*/
+	_Priority: [
+		ObjectType.UNIT
+		,ObjectType.CLS
+		,ObjectType.WEAPON
+		,ObjectType.ITEM
+		,ObjectType.SKILL
+		,ObjectType.TERRAIN
+		,ObjectType.STATE
+	],
 	
 	//名称取得
 	getName: function(type) {
@@ -94,6 +108,26 @@ var AttributeControl = {
 			if(this.isShow(i)) count2++;
 		}
 		return count2;
+	},
+	
+	//優先度
+	getPriority: function(index) {
+		return this._Priority[index];
+	},
+	
+	getPriorityCount: function() {
+		return this._Priority.length;
+	},
+	
+	//objのnullチェック+属性の設定がされているか
+	_isAttributeCustom: function(obj) {
+		if(obj == null) {
+			return false;
+		}
+		if(typeof obj.custom.attribute === 'undefined') {
+			return false;
+		}
+		return true;
 	},
 	
 	//objのカスパラから攻撃属性の種類を取得
@@ -165,61 +199,77 @@ var AttributeControl = {
 	
 	//ユニットの攻撃属性の種類を取得
 	getUnitAttackType: function(unit) {
-		var list, count, unitClass, weapon, item, skill, terrain, state;
+		var i, list, count, unitClass, weapon, item, skill, terrain, state;
 		var type = 0;
 		
-		//typeは上書き(優先順位は後のものほど高い)
-		//ユニット
-		if(this.getAttackType(unit) !== 0) {
-			type = this.getAttackType(unit);
-		}
+		//typeは上書き(優先度順)
+		var ip, objectType;
+		var count2 = this.getPriorityCount();
 		
-		//クラス
-		unitClass = unit.getClass();
-		if(this.getAttackType(unitClass) !== 0) {
-			type = this.getAttackType(unitClass);
-		}
-		
-		//武器・アイテム
-		weapon = ItemControl.getEquippedWeapon(unit);
-		if(this.getAttackType(weapon) !== 0) {
-			type = this.getAttackType(weapon);
-		}
-		count = UnitItemControl.getPossessionItemCount(unit);
-		for(i=0; i<count; i++) {
-			item = UnitItemControl.getItem(unit, i);
-			if(item != null && !item.isWeapon() && ItemControl.isItemUsable(unit, item)) {
-				if(this.getAttackType(item) !== 0) {
-					type = this.getAttackType(item);
+		for(ip=0; ip<count2; ip++) {
+			objectType = this.getPriority(ip);
+			if(objectType === ObjectType.UNIT) {
+				//ユニット
+				if(this.getAttackType(unit) !== 0) {
+					type = this.getAttackType(unit);
 				}
 			}
-		}
-		
-		//スキル
-		list = SkillControl.getSkillMixArray(unit, weapon, -1, '');
-		count = list.length;
-		for(i=0; i<count; i++) {
-			skill = list[i].skill;
-			if(this.getAttackType(skill) !== 0) {
-				type = this.getAttackType(skill);
+			else if(objectType === ObjectType.CLS) {
+				//クラス
+				unitClass = unit.getClass();
+				if(this.getAttackType(unitClass) !== 0) {
+					type = this.getAttackType(unitClass);
+				}
 			}
-		}
-		
-		//地形
-		terrain = PosChecker.getTerrainFromPos(unit.getMapX(), unit.getMapY());
-		if (terrain !== null) {
-			if(this.getAttackType(terrain) !== 0) {
-				type = this.getAttackType(terrain);
+			else if(objectType === ObjectType.WEAPON) {
+				//武器
+				weapon = ItemControl.getEquippedWeapon(unit);
+				if(this.getAttackType(weapon) !== 0) {
+					type = this.getAttackType(weapon);
+				}
 			}
-		}
-		
-		//ステート
-		list = unit.getTurnStateList();
-		count = list.getCount();
-		for(i=0; i<count; i++) {
-			state = list.getData(i).getState();
-			if(this.getAttackType(state) !== 0) {
-				type = this.getAttackType(state);
+			else if(objectType === ObjectType.ITEM) {
+				//アイテム
+				count = UnitItemControl.getPossessionItemCount(unit);
+				for(i=0; i<count; i++) {
+					item = UnitItemControl.getItem(unit, i);
+					if(item != null && !item.isWeapon() && ItemControl.isItemUsable(unit, item)) {
+						if(this.getAttackType(item) !== 0) {
+							type = this.getAttackType(item);
+						}
+					}
+				}
+			}
+			else if(objectType === ObjectType.SKILL) {
+				//スキル
+				list = SkillControl.getSkillMixArray(unit, weapon, -1, '');
+				count = list.length;
+				for(i=0; i<count; i++) {
+					skill = list[i].skill;
+					if(this.getAttackType(skill) !== 0) {
+						type = this.getAttackType(skill);
+					}
+				}
+			}
+			else if(objectType === ObjectType.TERRAIN) {
+				//地形
+				terrain = PosChecker.getTerrainFromPos(unit.getMapX(), unit.getMapY());
+				if (terrain !== null) {
+					if(this.getAttackType(terrain) !== 0) {
+						type = this.getAttackType(terrain);
+					}
+				}
+			}
+			else if(objectType === ObjectType.STATE) {
+				//ステート
+				list = unit.getTurnStateList();
+				count = list.getCount();
+				for(i=0; i<count; i++) {
+					state = list.getData(i).getState();
+					if(this.getAttackType(state) !== 0) {
+						type = this.getAttackType(state);
+					}
+				}
 			}
 		}
 		
@@ -228,7 +278,7 @@ var AttributeControl = {
 	
 	//ユニットの属性強化値の合計を取得
 	getUnitEnhance: function(unit, type) {
-		var list, count, unitClass, weapon, item, skill, terrain, state;
+		var i, list, count, unitClass, weapon, item, skill, terrain, state;
 		var value = this._BaseEnhanceValue;
 		
 		//ユニット
@@ -280,7 +330,7 @@ var AttributeControl = {
 	
 	//ユニットの属性耐性値の合計を取得
 	getUnitResist: function(unit, type) {
-		var list, count, unitClass, weapon, item, skill, terrain, state;
+		var i, list, count, unitClass, weapon, item, skill, terrain, state;
 		var value = this._BaseResistValue;
 		
 		//ユニット
